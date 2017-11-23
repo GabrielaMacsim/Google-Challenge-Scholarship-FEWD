2cf7d30f1d2a563b41c5a73e4ae9d1176d5953ef
Gabriela Macsim <31318570+GabrielaMacsim@users.noreply.github.com>
2017-11-23 16:06:11 +0200

    Create estherOriginal.js
    the first, simple Esther card

---
+++ estherOriginal.js
@@ -0,0 +1,24 @@
+#principal {
+ width: 300px;
+ border: 1px solid lightgray;
+ padding: 10px;
+ box-shadow: 8px 8px #F3F3F3;
+}
+
+#interesting {
+  font-style:italic;
+}
+
+#container {
+  border: 1px solid lightgray;
+  padding: 10px;
+}
+
+.image {
+ border: 1px solid lightgray;
+  width: 300px;
+}
+
+.list {
+  list-style: none;
+}
