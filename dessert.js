d4e0aabbe3269ac1b274bd10e745316aa5830ccd
Gabriela Macsim <31318570+GabrielaMacsim@users.noreply.github.com>
2017-11-23 15:55:32 +0200

    Create dessert.js

---
+++ dessert.js
@@ -0,0 +1,8 @@
+var flavor = "vanilla";
+var vessel = "cone";
+var toppings = "peanuts";
+
+// Add your code here
+if ((flavor === "vanilla" || flavor === "chocolate") && (vessel === "cone" || vessel === "bowl") && (toppings === "sprinkles" || toppings === "peanuts")) {
+    console.log("I'd like two scoops of " + flavor + " ice cream in a " + vessel + " with " + toppings + ".");
+}
